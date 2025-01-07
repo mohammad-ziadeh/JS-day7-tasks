0834ff9bd4e60d5861b632309505c8fa41577be9
mohammad <mohammedziadeh6@gmail.com>
2025-01-07 12:02:17 +0300

    Add files via upload

--- JS tasks day7/task2/main.js
+++ JS tasks day7/task2/main.js
@@ -1,41 +1,75 @@
-const form = document.getElementById("form");
-const input = document.getElementById("Tasks");
-const list = document.getElementById("list");
+class ToDo {
+  constructor() {
+    this.taskInput = document.getElementById("taskinput");
+    this.addBtn = document.querySelector("#addTask");
+    this.taskList = document.getElementById("taskList");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let items = input.value.trim();
-  if (items !== "") {
-    let newItem = document.createElement("li");
-    newItem.textContent = items;
-    list.appendChild(newItem);
-    //##############################################################
-    let comBtn = document.createElement("button");
-    comBtn.setAttribute("class", "comBtn");
-    comBtn.innerHTML = "Complete";
-    newItem.appendChild(comBtn);
-    comBtn.style.backgroundColor = "green";
-
-    comBtn.addEventListener("click", () => {
-      list.style.color = "grey";
-      list.style.textDecoration = "line-through";
-      comBtn.style.textDecoration="none"
+    this.taskInput.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        this.addBtn.click();
+      }
     });
-
-    //##############################################################
-    let delBtn = document.createElement("button");
-    delBtn.setAttribute("class", "delBtn");
-    delBtn.innerHTML = "delete";
-    newItem.appendChild(delBtn);
-    delBtn.style.backgroundColor = "red";
-
-    delBtn.addEventListener("click", () => {
-      list.removeChild(newItem);
+    this.addBtn.addEventListener("click", () => {
+      this.addTask();
     });
-    //##########################################################################
-
-    input.value = "";
-    input.focus();
   }
-});
+
+  addTask() {
+    const inputTask = this.taskInput.value;
+
+    if (inputTask == "") {
+      let Msg = [
+        "It's empty ಠ╭╮ಠ",
+        "Please enter something (´・ω・`)",
+        "So... No tasks? (･ัω･ั)?",
+        "I'm watching IT'S STILL EMPTY ಠ╭╮ಠ",
+      ];
+      window.alert(Msg[Math.floor(Math.random() * Msg.length)]);
+      return;
+    }
+
+    const listItem = document.createElement("li");
+    const inputSpan = document.createElement("span");
+    const completedBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+
+    completedBtn.addEventListener("click", () => {
+      this.toggleComplete(inputSpan);
+    });
+
+    deleteBtn.addEventListener("click", () => {
+      this.deleteTask(listItem);
+    });
+
+    completedBtn.classList.add("btn");
+    completedBtn.classList.add("btn-success");
+
+    deleteBtn.classList.add("btn");
+    deleteBtn.classList.add("btn-danger");
+
+    listItem.classList.add("mb-4");
+
+    inputSpan.innerText = inputTask;
+    completedBtn.innerText = "complete";
+    deleteBtn.innerText = "delete";
+
+    listItem.appendChild(inputSpan);
+    listItem.appendChild(completedBtn);
+    listItem.appendChild(deleteBtn);
+
+    taskList.appendChild(listItem);
+
+    this.taskInput.value = "";
+  }
+
+  toggleComplete(task) {
+    task.classList.toggle("completed");
+  }
+
+  deleteTask(task) {
+    task.remove();
+  }
+}
+
+const todoTask = new ToDo();
